de4bc9c16bf02ae19aaab481322486e16703bd70
brianrono <brianrono16@gmail.com>
2023-04-26 22:17:33 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ function App() {
     1: { id: 1, title: "A River Runs Through It" },
     2: { id: 2, title: "Se7en" },
     3: { id: 3, title: "Inception" },
+    4: { id: 4, title: "John Wick" },
   });
 
   return (
